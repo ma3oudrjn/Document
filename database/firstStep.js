544b04c6521b37dbbab588219239bc1d97a4c2f1
masoud <111561777+ma3oudrjn@users.noreply.github.com>
2023-08-17 12:02:16 +0330

    Deleted

--- database/firstStep.js
+++
@@ -1,7 +0,0 @@
-const db = require('mongoose')
-db.connect("mongodb+srv://ma3oud:138162@cluster0.irtsiue.mongodb.net/")
-.then(()=>{
-console.log("you are connect")
-}).catch((err)=>{
-console.log("eror to connect to the data base",err)
-})
