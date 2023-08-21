4f1f4784128fe11fa9fd50ee3fd7da88ec06f134
masoud <masoudrng2@gmail.com>
2023-08-21 15:15:35 +0330

    update serverConfigs

--- jwt/server.js
+++ jwt/server.js
@@ -1,30 +1,55 @@
-require("dotenv").config();
 
-const express = require ('express');
-const app = express();
+const express = require('express');
+const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
-console.log("server run");
-app.use(express.json())
-const posts = [
-{
-    username: "naser",
-    title: "number 1 user"
-},
-{
-    username: "nader",
-    title: "number 2 user"
-}]
-app.get('/posts',(req,res)=>{
-res.json(posts)
-})
-
-app.post('/login',(req,res)=>{
-const username = req.body.username
-const user = {user: username}
-const accessToken=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET)
-res.json=({ accessToken: accessToken })
-})
-
-
-
-app.listen(3030)
+  
+const app = express();
+  
+// Set up Global configuration access
+dotenv.config();
+  
+let PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is up and running on ${PORT} ...`);
+});
+  
+// Main Code Here  //
+// Generating JWT
+app.post("/user/generateToken", (req, res) => {
+    // Validate User Here
+    // Then generate JWT Token
+  
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+    let data = {
+        time: Date(),
+        userId: 12,
+    }
+  
+    const token = jwt.sign(data, jwtSecretKey);
+  
+    res.send(token);
+});
+  
+// Verification of JWT
+app.get("/user/validateToken", (req, res) => {
+    // Tokens are generally passed in header of request
+    // Due to security reasons.
+  
+    let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+  
+    try {
+        const token = req.header(tokenHeaderKey);
+  
+        const verified = jwt.verify(token, jwtSecretKey);
+        if(verified){
+            return res.send("Successfully Verified");
+        }else{
+            // Access Denied
+            return res.status(401).send(error);
+        }
+    } catch (error) {
+        // Access Denied
+        return res.status(401).send(error);
+    }
+});
