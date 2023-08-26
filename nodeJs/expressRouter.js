af26491ba89a536fba96f408855034e97eb19dae
masoud <masoudrng2@gmail.com>
2023-08-26 08:42:08 +0330

    add port

--- nodeJs/expressRouter.js
+++ nodeJs/expressRouter.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const app = express()
 const route= express.Router
+const PORT = 3000;
+app.listen(PORT,(err)=>{if(err) console.log(err)
+    else console.log("you are listen to: ",PORT,'port');
+})
+
+
+
