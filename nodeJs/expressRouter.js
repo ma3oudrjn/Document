3503f9799df262c0e8e722dd89308e0f338f8ce3
masoud <masoudrng2@gmail.com>
2023-08-26 08:43:52 +0330

    Root correction

--- nodeJs/expressRouter.js
+++ nodeJs/expressRouter.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express()
-const route= express.Router
+const router= express.Router
 const PORT = 3000;
+
+router.get('/', function (req, res, next) {
+    console.log("Router Working");
+    res.end();
+})
+ 
+app.use(router);
 app.listen(PORT,(err)=>{if(err) console.log(err)
     else console.log("you are listen to: ",PORT,'port');
 })
