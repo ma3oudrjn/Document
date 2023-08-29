aaafaa89dcef60a859bd4da13d2afd2c629c238c
masoud <masoudrng2@gmail.com>
2023-08-29 09:52:05 +0330

    add

--- someApi/models/abaoutUs.js
+++ someApi/models/abaoutUs.js
@@ -1,12 +1,11 @@
 
 const mongoose = require('mongoose');
-const { NUMBER } = require('sequelize');
 const Schema = mongoose.Schema;
   
-let abaoutUsSchema=new Schema({
+let aboutUsSchema=new Schema({
   content: { type:String,},
   urlImage:{type:String},
-  size:{type:NUMBER},
+  size:{type:Number},
   imageName:{type:String}
 }, {
     collection: 'about us'
