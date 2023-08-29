2dc2acae498cab9aef7c2518511c4daca1d106a7
masoud <masoudrng2@gmail.com>
2023-08-29 16:12:57 +0330

    final

--- someApi/routes/user.js
+++ someApi/routes/user.js
@@ -4,32 +4,4 @@ const mongoose= require('mongoose')
 const user = require('../models/user');
 ap = express.Router();
 
-//new user
-ap.post('/new/user',(req,res,next)=>{
-    user.create(req.body, (error, data) => {
-        if (error) {
-          return next(error);
-        } else {
-          console.log(data);
-          res.json(data);
-        }
-      });
-    });
 
-
-//delete user
-    ap.delete("/delete/user/:id", 
-    (req, res, next) => {
-      user.findByIdAndRemove(
-          req.params.id, (error, data) => {
-        if (error) {
-          return next(error);
-        } else {
-          res.status(200).json({
-            msg: data,
-          });
-        }
-      });
-    });
-
-      module.exports = ap;
--- someApi/validator/userValid.js
+++ someApi/validator/userValid.js
@@ -3,3 +3,33 @@ const app = express()
 const schema = require('../routes/user')
 const joi = require('joi')
 
+
+
+//new user
+ap.post('/new/user',(req,res,next)=>{
+    user.create(req.body, (error, data) => {
+        if (error) {
+          return next(error);
+        } else if(r) {
+          console.log(data);
+          res.json(data);
+        }
+      });
+    });
+
+
+//delete user
+    ap.delete("/delete/user/:id", 
+    (req, res, next) => {
+      user.findByIdAndRemove(
+          req.params.id, (error, data) => {
+        if (error) {
+          return next(error);
+        } else {
+          res.status(200).json({
+            msg: data,
+          });
+        }
+      });
+    });
+    module.exports = ap;
